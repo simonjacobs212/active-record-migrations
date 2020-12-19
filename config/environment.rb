676693edfe49a6470d1ac26d3a278c7fef553bcb
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-12-19 14:23:10 -0500

    commit tree 801465f485cc84d653a5737f572d1ed060318e44

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,12 @@ require 'bundler/setup'
 Bundler.require
 
 
-# put the code to connect to the database here
+ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/artists.sqlite"
+  ) 
 
 
 require_relative "../artist.rb"
+
+# RSpec.configure(&:disable_monkey_patching!)
